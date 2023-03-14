f9e1c41ff4eedad7a125f432022a40afc6e53ef6
Sxnty <sxntycsgo@gmail.com>
2023-03-14 07:49:57 -0300

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import { AnimalProvider } from "./context/Animals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <AnimalProvider>
-        <Navbar />
-        <Home />
-        <Footer />
-      </AnimalProvider>
+      <BrowserRouter>
+        <AnimalProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/footer" element={<Footer />} />
+          </Routes>
+          <Footer />
+        </AnimalProvider>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,25 +21,27 @@ function Home() {
   const nameFilter = (e) => {
     let animalName = e.target.value;
 
-    if(!animalName.length && isChecked.checked) {
+    if (!animalName.length && isChecked.checked) {
       // dame todo, filtrado por el tipo.
-      console.log('Todo, filtrado por tipo');
-      console.log(isChecked.name, 'type')
-      typeFilter(isChecked.checked,isChecked.name);
+      console.log("Todo, filtrado por tipo");
+      console.log(isChecked.name, "type");
+      typeFilter(isChecked.checked, isChecked.name);
     } else if (!animalName.length && !isChecked.checked) {
-      console.log('Todo, sin filtado por tipo');
+      console.log("Todo, sin filtado por tipo");
       setDataFiltered(fullData);
     } else {
-      let dataFiltrada = dataFiltered.filter(data => data.name.toLowerCase().includes(animalName.toLowerCase()));
-      console.log(dataFiltrada, 'data Filtrada');
-      if(dataFiltrada.length) {
+      let dataFiltrada = dataFiltered.filter((data) =>
+        data.name.toLowerCase().includes(animalName.toLowerCase())
+      );
+      console.log(dataFiltrada, "data Filtrada");
+      if (dataFiltrada.length) {
         setDataFiltered(dataFiltrada);
       }
     }
   };
 
-  const typeFilter = (checked,animalType) => {
-    if(checked) {
+  const typeFilter = (checked, animalType) => {
+    if (checked) {
       let dataFiltrada = fullData.filter(
         (data) => data.animal.toLowerCase() === animalType.toLowerCase()
       );
@@ -49,7 +51,6 @@ function Home() {
       // quitar filtro
       setDataFiltered(fullData);
     }
-   
   };
 
   return (
@@ -64,7 +65,7 @@ function Home() {
                 type="checkbox"
                 className="checkbox__input"
                 onClick={(e) => {
-                  typeFilter(e.target.checked,"dog");
+                  typeFilter(e.target.checked, "dog");
                 }}
               />
               <span className="checkbox__icon">
@@ -73,18 +74,26 @@ function Home() {
               </span>
             </label>
             <label className="filter__checkbox">
-              <input type="checkbox" className="checkbox__input" onClick={(e) => {
-                  typeFilter(e.target.checked,"cat");
-                }}/>
+              <input
+                type="checkbox"
+                className="checkbox__input"
+                onClick={(e) => {
+                  typeFilter(e.target.checked, "cat");
+                }}
+              />
               <span className="checkbox__icon">
                 <TbCat />
                 Gatos
               </span>
             </label>
             <label className="filter__checkbox">
-              <input type="checkbox" className="checkbox__input" onClick={(e) => {
-                  typeFilter(e.target.checked,"bird");
-                }}/>
+              <input
+                type="checkbox"
+                className="checkbox__input"
+                onClick={(e) => {
+                  typeFilter(e.target.checked, "bird");
+                }}
+              />
               <span className="checkbox__icon">
                 <GiHummingbird />
                 Aves
